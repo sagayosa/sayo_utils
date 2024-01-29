a1bc6b6cec2cb484f88976520d5d83e01a4dd904
sagayosa <gyjgrteen@gmail.com>
2024-01-29 14:07:56 +0800

    add FillSameField

--- utils/utils.go
+++ utils/utils.go
@@ -10,6 +10,7 @@ import (
 	"net/http"
 	"net/url"
 	"os"
+	"reflect"
 	"strings"
 )
 
@@ -125,4 +126,25 @@ func GetAvailablePort() (int, error) {
 	defer listener.Close()
 	addr := listener.Addr().(*net.TCPAddr)
 	return addr.Port, nil
+}
+
+func FillSameField(source interface{}, dest interface{}) {
+	valSource := reflect.ValueOf(source)
+	valDest := reflect.ValueOf(dest)
+
+	if valSource.Kind() == reflect.Ptr {
+		valSource = valSource.Elem()
+	}
+	if valDest.Kind() == reflect.Ptr {
+		valDest = valDest.Elem()
+	}
+
+	for i := 0; i < valSource.NumField(); i++ {
+		fieldSource := valSource.Field(i)
+		fieldDest := valDest.FieldByName(valSource.Type().Field(i).Name)
+
+		if fieldDest.IsValid() && fieldDest.CanSet() && fieldSource.Kind() == fieldDest.Kind() {
+			fieldDest.Set(fieldSource)
+		}
+	}
 }
