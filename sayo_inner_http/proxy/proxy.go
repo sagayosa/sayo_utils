391194877fa5019e46bac2c94f9138797114497f
sagayosa <gyjgrteen@gmail.com>
2024-03-08 16:53:10 +0800

    NewWindow will return the uuid

--- sayo_inner_http/proxy/proxy.go
+++ sayo_inner_http/proxy/proxy.go
@@ -88,25 +88,25 @@ type NewWindowReq struct {
 	Option interface{} `json:"option"`
 }
 
-func NewWindow(frameworkAddr string, req *NewWindowReq) error {
+func NewWindow(frameworkAddr string, req *NewWindowReq) (string, error) {
 	url := utils.StringPlus("http://", frameworkAddr, constant.ProxyDesktopNewWindowURL)
 	code, body, err := utils.Post(url, req)
 	if err != nil {
-		return err
+		return "", err
 	}
 	if code != http.StatusOK {
-		return sayoerror.ErrorInStatusCode(sayoerror.ErrNewWindowFailed, code)
+		return "", sayoerror.ErrorInStatusCode(sayoerror.ErrNewWindowFailed, code)
 	}
 
 	resp := &baseresp.BaseResp{}
 	if err = json.Unmarshal(body, resp); err != nil {
-		return err
+		return "", err
 	}
 	if resp.Code != sayoerror.SuccessCode {
-		return sayoerror.ErrorInMsgCode(sayoerror.ErrNewWindowFailed, int(resp.Code), resp.Msg)
+		return "", sayoerror.ErrorInMsgCode(sayoerror.ErrNewWindowFailed, int(resp.Code), resp.Msg)
 	}
 
-	return nil
+	return resp.Data.(string), nil
 }
 
 func OpenFileSelector(frameworkAddr string) (result string, err error) {
