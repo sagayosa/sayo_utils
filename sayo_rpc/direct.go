f4619841d6c7579eba5ca721f66a34c0b437e17b
sagayosa <gyjgrteen@gmail.com>
2024-03-18 12:36:39 +0800

    add direct

--- sayo_rpc/direct.go
+++ sayo_rpc/direct.go
@@ -1,1 +1,19 @@
 package sayorpc
+
+import "github.com/grteen/sayo_utils/utils"
+
+func DirectGet(addr string, uri string, data map[string]interface{}) (code int, body []byte, err error) {
+	return Get(utils.StringPlus("http://", addr, uri), data)
+}
+
+func DirectPost(addr string, uri string, data interface{}) (code int, body []byte, err error) {
+	return Post(utils.StringPlus("http://", addr, uri), data)
+}
+
+func DirectPut(addr string, uri string, data interface{}) (code int, body []byte, err error) {
+	return Put(utils.StringPlus("http://", addr, uri), data)
+}
+
+func DirectDelete(addr string, uri string, data interface{}) (code int, body []byte, err error) {
+	return Delete(utils.StringPlus("http://", addr, uri), data)
+}
