86b7ee1597fc2bd0689daaaa56e47f14964cc826
sagayosa <gyjgrteen@gmail.com>
2024-03-30 11:44:21 +0800

    add GenerateURL

--- utils/utils.go
+++ utils/utils.go
@@ -78,90 +78,6 @@ func JSONPersistence(filePath string, source interface{}) error {
 	return os.WriteFile(filePath, prettyJSON.Bytes(), 0777)
 }
 
-// func httpRequest(way string, URL string, data interface{}) (code int, body []byte, err error) {
-// 	if data == nil {
-// 		data = struct{}{}
-// 	}
-// 	bts, err := json.Marshal(data)
-// 	if err != nil {
-// 		return
-// 	}
-
-// 	req, err := http.NewRequest(way, URL, bytes.NewBuffer(bts))
-// 	if err != nil {
-// 		return
-// 	}
-
-// 	req.Header.Set("Content-Type", "application/json")
-
-// 	client := &http.Client{}
-// 	resp, err := client.Do(req)
-// 	if err != nil {
-// 		return
-// 	}
-// 	defer resp.Body.Close()
-
-// 	body, err = io.ReadAll(resp.Body)
-// 	if err != nil {
-// 		return
-// 	}
-
-// 	return resp.StatusCode, body, nil
-// }
-
-// func Put(URL string, data interface{}) (code int, body []byte, err error) {
-// 	return httpRequest("PUT", URL, data)
-// }
-
-// func Post(URL string, data interface{}) (code int, body []byte, err error) {
-// 	// bts, err := json.Marshal(data)
-// 	// if err != nil {
-// 	// 	return
-// 	// }
-
-// 	// resp, err := http.Post(URL, "application/json", bytes.NewBuffer(bts))
-// 	// if err != nil {
-// 	// 	return
-// 	// }
-
-// 	// defer resp.Body.Close()
-// 	// body, err = io.ReadAll(resp.Body)
-// 	// if err != nil {
-// 	// 	return
-// 	// }
-
-// 	// return resp.StatusCode, body, nil
-// 	return httpRequest("POST", URL, data)
-// }
-
-// func Get(URL string, data map[string]interface{}) (code int, body []byte, err error) {
-// 	if data == nil {
-// 		data = make(map[string]interface{})
-// 	}
-// 	params := url.Values{}
-// 	for k, v := range data {
-// 		params.Add(k, fmt.Sprintf("%v", v))
-// 	}
-
-// 	reqURL, err := url.Parse(URL)
-// 	if err != nil {
-// 		return
-// 	}
-// 	reqURL.RawQuery = params.Encode()
-
-// 	resp, err := http.Get(reqURL.String())
-// 	if err != nil {
-// 		return
-// 	}
-// 	defer resp.Body.Close()
-
-// 	body, err = io.ReadAll(resp.Body)
-// 	if err != nil {
-// 		return
-// 	}
-// 	return resp.StatusCode, body, nil
-// }
-
 func UnMarshalUnknownAny(source interface{}, dest interface{}) error {
 	bts, err := json.Marshal(source)
 	if err != nil {
@@ -254,4 +170,8 @@ func Debounce(fn func(), delay time.Duration) func() {
 			mutex.Unlock()
 		})
 	}
+}
+
+func GenerateURL(addr string, url string) string {
+	return StringPlus("http://", addr, "/", url)
 }
