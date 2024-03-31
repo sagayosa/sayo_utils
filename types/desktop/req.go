5c0a684f771448cb906981b756b7d98f2cd19597
sagayosa <gyjgrteen@gmail.com>
2024-03-31 17:14:07 +0800

    add newwindowreq

--- types/desktop/req.go
+++ types/desktop/req.go
@@ -3,3 +3,12 @@ package desktoptypes
 // GET /fileselector
 type OpenFileSelectorReq struct {
 }
+
+// POST /window
+type NewWindowReq struct {
+	Theme    string      `json:"theme"`
+	Url      string      `json:"url"`
+	Frame    bool        `json:"frame"`
+	Dragable bool        `json:"dragable"`
+	Option   interface{} `json:"option"`
+}
