d57619cf4d76ffde726bdcf2a1f9a36c2e83fb94
sagayosa <gyjgrteen@gmail.com>
2024-03-31 11:14:52 +0800

    add ai resp

---
+++ types/ai/other.go
@@ -0,0 +1,26 @@
+package aitypes
+
+type CompletionsAIResp struct {
+	ID                string    `json:"id"`
+	Object            string    `json:"object"`
+	Created           int       `json:"created"`
+	Model             string    `json:"model"`
+	Choices           []Choices `json:"choices"`
+	Usage             Usage     `json:"usage"`
+	SystemFingerprint string    `json:"system_fingerprint"`
+}
+type Message struct {
+	Role    string `json:"role"`
+	Content string `json:"content"`
+}
+type Choices struct {
+	Index        int     `json:"index"`
+	Message      Message `json:"message"`
+	Logprobs     any     `json:"logprobs"`
+	FinishReason string  `json:"finish_reason"`
+}
+type Usage struct {
+	PromptTokens     int `json:"prompt_tokens"`
+	CompletionTokens int `json:"completion_tokens"`
+	TotalTokens      int `json:"total_tokens"`
+}
--- types/ai/resp.go
+++ types/ai/resp.go
@@ -1,27 +1,12 @@
 package aitypes
 
-// POST chat/completions
-type CompletionsAIResp struct {
-	ID                string    `json:"id"`
-	Object            string    `json:"object"`
-	Created           int       `json:"created"`
-	Model             string    `json:"model"`
-	Choices           []Choices `json:"choices"`
-	Usage             Usage     `json:"usage"`
-	SystemFingerprint string    `json:"system_fingerprint"`
-}
-type Message struct {
+// chat/completions
+type CompletionsResp struct {
 	Role    string `json:"role"`
 	Content string `json:"content"`
 }
-type Choices struct {
-	Index        int     `json:"index"`
-	Message      Message `json:"message"`
-	Logprobs     any     `json:"logprobs"`
-	FinishReason string  `json:"finish_reason"`
-}
-type Usage struct {
-	PromptTokens     int `json:"prompt_tokens"`
-	CompletionTokens int `json:"completion_tokens"`
-	TotalTokens      int `json:"total_tokens"`
+
+// POST chat/decision
+type DecisionResp struct {
+	Content string `json:"content"`
 }
